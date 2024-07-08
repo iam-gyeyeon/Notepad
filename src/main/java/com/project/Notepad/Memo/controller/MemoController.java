f591a3592c7f575dbbc7b3fb0afeb436e84ef3da
gyeyeon <iam.gyeyeon@gmail.com>
2024-07-08 19:35:49 +0900

    DB 연결에 필요한 라이브러리, properties 추가 api 호출 부 수정

--- src/main/java/com/project/Notepad/Memo/controller/MemoController.java
+++ src/main/java/com/project/Notepad/Memo/controller/MemoController.java
@@ -3,52 +3,72 @@ package com.project.Notepad.Memo.controller;
 import com.project.Notepad.Memo.dto.MemoRequestDto;
 import com.project.Notepad.Memo.dto.MemoResponseDto;
 import com.project.Notepad.Memo.entity.Memo;
-import org.springframework.stereotype.Controller;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class MemoController {
 
-    private final Map<Long, Memo> memoList = new HashMap<>();
+    private final JdbcTemplate jdbcTemplate;
 
-    /**
-     * @param requestDto
-     * @return
-     */
+    public MemoController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @PostMapping("/memos")
-    public MemoResponseDto addMemo(@RequestBody MemoRequestDto requestDto) {
-        //RequestDto -> Entity
-        Memo memo = new Memo(requestDto);
+    public MemoResponseDto createMemo(@RequestBody MemoRequestDto memoRequestDto) {
+        Memo memo = new Memo(memoRequestDto);
 
-        //get Memo max Id
-        Long maxId = !memoList.isEmpty() ? Collections.max(memoList.keySet()) + 1 : 1;
-        memo.setId(maxId);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
 
-        //save data
-        memoList.put(memo.getId(), memo);
+        String sql = "INSERT INTO memo (username, contents) VALUES (?, ?)";
 
-        //Entity -> ResponseDto
+        jdbcTemplate.update(con -> {
+            PreparedStatement ps = con.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setString(1, memo.getUsername());
+            ps.setString(2, memo.getContents());
+            return ps;
+        }, keyHolder);
 
-        return new MemoResponseDto(memo);
+        Long id = keyHolder.getKey().longValue();
+        memo.setId(id);
+
+        MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
+        return memoResponseDto;
     }
 
     @GetMapping("/memos")
     public List<MemoResponseDto> getMemos() {
-        //Map to List
-        List<MemoResponseDto> responseList = memoList.values().stream().map(MemoResponseDto::new).toList();
+        String sql = "SELECT * FROM memo";
 
-        return responseList;
+        return jdbcTemplate.query(sql, new RowMapper<MemoResponseDto>() {
+            @Override
+            public MemoResponseDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Long id = rs.getLong("id");
+                String username = rs.getString("username");
+                String contents = rs.getString("contents");
+                return new MemoResponseDto(id, username, contents);
+            }
+        });
     }
 
     @PutMapping("/memos/{id}")
-    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
-        if (memoList.containsKey(id)) {
-            Memo memo = memoList.get(id);
-            memo.update(requestDto);
-            return memo.getId();
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto memoRequestDto) {
+
+        Memo memo = findById(id);
+        if(memo != null){
+            String sql = "UPDATE memo SET username=?, contents=? WHERE id=?";
+            jdbcTemplate.update(sql, memoRequestDto.getUsername(), memoRequestDto.getContents(), id);
+            return id;
         }else{
             throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
         }
@@ -56,11 +76,30 @@ public class MemoController {
 
     @DeleteMapping("/memos/{id}")
     public Long deleteMemo(@PathVariable Long id) {
-        if(memoList.containsKey(id)) {
-            memoList.remove(id);
+        Memo memo = findById(id);
+        if(memo != null ){
+            String sql = "DELETE FROM memo WHERE id=?";
+            jdbcTemplate.update(sql, id);
             return id;
         }else{
             throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
         }
     }
+
+    private Memo findById(Long id) {
+        String sql = "SELECT * FROM memo WHERE id = ?";
+
+        return jdbcTemplate.query(sql, resultSet -> {
+            if (resultSet.next()) {
+                Memo memo = new Memo();
+                memo.setUsername(resultSet.getString("username"));
+                memo.setContents(resultSet.getString("contents"));
+                return memo;
+            } else {
+                return null;
+            }
+        }, id);
+    }
+
 }
+
---
+++ src/main/java/com/project/Notepad/Memo/controller/MemoController_bak.java
@@ -0,0 +1,64 @@
+package com.project.Notepad.Memo.controller;
+
+import com.project.Notepad.Memo.dto.MemoRequestDto;
+import com.project.Notepad.Memo.dto.MemoResponseDto;
+import com.project.Notepad.Memo.entity.Memo;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+
+public class MemoController_bak {
+
+    private final Map<Long, Memo> memoList = new HashMap<>();
+
+    /**
+     * @param requestDto
+     * @return
+     */
+    @PostMapping("/memos")
+    public MemoResponseDto addMemo(@RequestBody MemoRequestDto requestDto) {
+        //RequestDto -> Entity
+        Memo memo = new Memo(requestDto);
+
+        //get Memo max Id
+        Long maxId = !memoList.isEmpty() ? Collections.max(memoList.keySet()) + 1 : 1;
+        memo.setId(maxId);
+
+        //save data
+        memoList.put(memo.getId(), memo);
+
+        //Entity -> ResponseDto
+
+        return new MemoResponseDto(memo);
+    }
+
+    @GetMapping("/memos")
+    public List<MemoResponseDto> getMemos() {
+        //Map to List
+        List<MemoResponseDto> responseList = memoList.values().stream().map(MemoResponseDto::new).toList();
+
+        return responseList;
+    }
+
+    @PutMapping("/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+        if (memoList.containsKey(id)) {
+            Memo memo = memoList.get(id);
+            memo.update(requestDto);
+            return memo.getId();
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
+    @DeleteMapping("/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        if (memoList.containsKey(id)) {
+            memoList.remove(id);
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+}
